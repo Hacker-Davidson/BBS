789aaa4b504ecced24e675b65a453f8e45b9bc24
kanta-1112 <131357879+kanta-1112@users.noreply.github.com>
2024-03-09 15:57:18 +0900

    二重送信のバグ解消

--- index.php
+++ index.php
@@ -17,6 +17,8 @@ $message_array = array();
 $success_message = null;
 $error_message = array();
 
+session_start();
+
 if(!empty($_FILES)){
     #$_FILESからファイル名取得
     $filename = $_FILES['upload_image']['name'];
@@ -59,9 +61,13 @@ if ( !empty($_POST['btn_submit']) ){
             //ファイルを閉じる
             fclose($file_handle);
 
-            $success_message = 'メッセージを書き込みました。';
+       
         }
-    }
+
+        $_SESSION['success_message'] = 'メッセージを書き込みました。';
+        header('Location: ./');
+		exit();
+        }
 
 }
 
@@ -87,21 +93,27 @@ if( $file_handle = fopen(FILENAME,'r') ) {
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>ひと言掲示板</title>
+<title>BBS</title>
 <style>
+    #innerBody{margin:auto;position:relative;top:360px;/*background:red;*/}
+    #form-box{height:350px;background:#FFF;width:95%;right:0;left:0;margin:auto;bottom:30px;position:relative;border:solid 3px #000;border-radius:40px;}
+    .form-box0{width:92%;margin:auto;}
+    .title{font-size:26px;font-weight:bolder;right:0;left:0;}
+    input, textarea{border:2px solid #000; box-sizing:border-box;}
+    #message{height:8em;font-size:10px;}
     #box{overflow-y:scroll;height:700px;background:#FFF;width:95%;right:0;left:0;margin:auto;top:10px;position:relative;border:solid 4px #000;border-radius:40px;}
     #moveBtn{
-        font-size:40px;color:#FFF;background-color:#00CCCC;
-        right:0;left:35%;bottom:65px;margin:auto;padding:5px;position:relative;border-radius:100%;text-align:center;height:52px;width:52px;z-index:2;}
+        font-size:75px;color:#FFF;background-color:#00CCCC;
+        right:0;left:35%;bottom:65px;margin:auto;padding:5px;position:relative;border-radius:100%;text-align:center;height:92px;width:92px;z-index:2;}
     .showImg{width:95%;}
     .time{font-size:20px;background:#ffcc00;}
     .lineHeight{line-height:0.5px;}
     table{border-bottom:solid 2px #000; /*background:red;*/}
     td{flex-wrap: wrap;}
     .box0{width:95%; position:relative; right:0; left:0; margin:auto;/*background:skyblue;*/}
-    .td0{width:15%; /*background:red;*/} /*time*/
-    .td1{width:65%; /*background:yellow;*/} /*msg*/
-    .td2{width:20%; text-align:center; /*background:green;*/} /*photo*/
+    .td0{width:15%; font-size:24px;} /*time*/
+    .td1{width:65%;} /*msg*/ .msg{font-size:30px;}
+    .td2{width:20%; text-align:center;} /*photo*/
     .usrName{line-height:1px;}
 </style>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
@@ -109,62 +121,66 @@ if( $file_handle = fopen(FILENAME,'r') ) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
 </head>
 <body>
-<h1>ひと言掲示板</h1>
-<?php if( !empty($success_message) ): ?>
-    <p class="success_message"><?php echo $success_message; ?></p>
-<?php endif; ?>
-<?php if( !empty($error_message) ): ?>
-	<ul class="error_message">
-		<?php foreach( $error_message as $value ): ?>
-			<li><?php echo $value; ?></li>
-		<?php endforeach; ?>
-	</ul>
-<?php endif; ?>
-<form method="post", enctype = "multipart/form-data">
-	<div>
-		<label for="view_name">表示名</label>
-		<input id="view_name" type="text" name="view_name" value="">
-	</div>
-	<div>
-		<label for="message">ひと言メッセージ</label>
-		<textarea id="message" name="message"></textarea>
-	</div>
-    <div>
-    <input type = "file", name = "upload_image">
-    </div>
-	<input type="submit" name="btn_submit" value="書き込む">
-</form>
-<!-- ここにメッセージの入力フォームを設置 -->
-<hr>
-<div id="box">
-    <!-- ここに投稿されたメッセージを表示 -->
-    <div class="box0">
-        <?php if( !empty($message_array) ): ?>
-        <?php foreach( $message_array as $value ): ?>
-            <table>
-                <!--<h2><?php echo $value['view_name']; ?></h2>-->
-                <td class="td0">
-                    <p><?php echo date('m月d日', strtotime($value['post_date'])); ?></p>
-                    <p class="lineHeight"><?php echo date('H:i', strtotime($value['post_date'])); ?></p>
-                </td>
-                <td class="td1">
-                    <h2 class="usrName"><?php echo $value['view_name']; ?></h2>
-                    <?php echo $value['message']; ?>
-                </td><!--メッセージを表示しているところ-->
-                <!--画像を表示している箇所-->
-                <td class="td2">
-                    <!--<?php if(!empty($MSG)) echo $MSG;?>--><!--画像のファイル名, 後で削除-->
-                        <?php if(!empty($img_path)){;?>
+<div id="innerBody">
+    <?php if( !empty($error_message) ): ?>
+        <ul class="error_message">
+            <?php foreach( $error_message as $value ): ?>
+                <li><?php echo $value; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    <form method="post", enctype = "multipart/form-data">
+        <div id="form-box">
+            <p class="form-box0"><br>
+                <label class="title" for="view_name">name</label>
+                <input id="view_name" type="text" name="view_name" value="" style="width:50%;">
+            </p>
+            <p class="form-box0">
+                <label class="title" for="message">message</label><br>
+                <textarea id="message" name="message" style="width:100%;"></textarea>
+            </p>
+            <p class="form-box0">
+                <input type="file" name="upload_image" style="font-size:20px; border-radius:50px;">
+                <input type="submit" name="btn_submit" style="font-size:20px; border-radius:50px;" value="書き込む"><br>
+                <?php if( empty($_POST['btn_submit']) && !empty($_SESSION['success_message']) ): ?>
+                    <span class="success_message">
+                        <?php echo htmlspecialchars( $_SESSION['success_message'], ENT_QUOTES, 'UTF-8'); ?></span>
+                    <?php unset($_SESSION['success_message']); ?>
+                <?php endif; ?>
+            </p>
+        </div>
+    </form>
+    <!-- ここにメッセージの入力フォームを設置 -->
+    <div id="box">
+        <!-- ここに投稿されたメッセージを表示 -->
+        <div class="box0">
+            <?php if( !empty($message_array) ): ?>
+            <?php foreach( $message_array as $value ): ?>
+                <table>
+                    <td class="td0"><!--投稿時間-->
+                        <p><?php echo date('m月d日', strtotime($value['post_date'])); ?></p>
+                        <p class="lineHeight"><?php echo date('H:i', strtotime($value['post_date'])); ?></p>
+                    </td>
+
+                    <td class="td1"><!--メッセージを表示しているところ-->
+                        <h1 class="usrName"><?php echo $value['view_name']; ?></h1>
+                        <p class="msg"><?php echo $value['message']; ?></p>
+                    </td>
+
+                    <td class="td2"><!--画像を表示している箇所-->
+                        <!--<?php if(!empty($MSG)) echo $MSG;?>--><!--画像のファイル名, 後で削除-->
+                        <?php if(!empty ($value['img_data'])){;?>
                             <a href="<?php echo $value['img_data'];?>" data-lightbox="group"><img class="showImg" src = "<?php echo $value['img_data'];?>" alt="">
                         <?php }; ?>
-                </td>
-            </table>
-        <?php endforeach; ?>
-        <?php endif; ?>
+                    </td>
+                </table>
+            <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
 
-<p id="moveBtn" onclick="moveTop()">↑</p><!--スクロール用ボタン-->
+    <p id="moveBtn" onclick="moveTop()">↑</p><!--スクロール用ボタン-->
+</div>
 <script>
     let box=document.getElementById("box");
     let topBtn=class{
