e00a7f4799ac7e35f24bdab7feaa5a34b9627613
夏純心 <junshin@XiaTianMacBook-Air.local>
2024-03-09 13:47:44 +0900

    feature:見た目調整

--- index.php
+++ index.php
@@ -87,10 +87,12 @@ if( $file_handle = fopen(FILENAME,'r') ) {
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>ひと言掲示板</title>
+<title>BBS</title>
 <style>
     #innerBody{margin:auto;position:relative;top:360px;/*background:red;*/}
-    #form-box{height:300px;background:#FFF;width:95%;right:0;left:0;margin:auto;bottom:30px;position:relative;border:solid 4px #000;border-radius:40px;}
+    #form-box{height:300px;background:#FFF;width:95%;right:0;left:0;margin:auto;bottom:30px;position:relative;border:solid 3px #000;border-radius:40px;}
+    #form-box0{background:red;}
+    input, textarea{border:2px solid #000;}
     #box{overflow-y:scroll;height:700px;background:#FFF;width:95%;right:0;left:0;margin:auto;top:10px;position:relative;border:solid 4px #000;border-radius:40px;}
     #moveBtn{
         font-size:75px;color:#FFF;background-color:#00CCCC;
@@ -101,9 +103,9 @@ if( $file_handle = fopen(FILENAME,'r') ) {
     table{border-bottom:solid 2px #000; /*background:red;*/}
     td{flex-wrap: wrap;}
     .box0{width:95%; position:relative; right:0; left:0; margin:auto;/*background:skyblue;*/}
-    .td0{width:15%; font-size:24px; /*background:red;*/} /*time*/
-    .td1{width:65%; /*background:yellow;*/} /*msg*/ .msg{font-size:30px;}
-    .td2{width:20%; text-align:center; /*background:green;*/} /*photo*/
+    .td0{width:15%; font-size:24px;} /*time*/
+    .td1{width:65%;} /*msg*/ .msg{font-size:30px;}
+    .td2{width:20%; text-align:center;} /*photo*/
     .usrName{line-height:1px;}
 </style>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
@@ -121,27 +123,29 @@ if( $file_handle = fopen(FILENAME,'r') ) {
     <?php endif; ?>
     <form method="post", enctype = "multipart/form-data">
         <table id="form-box">
-            <tr>
-                <td>
-                    <label for="view_name">name</label>
-                    <input id="view_name" type="text" name="view_name" value="">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="message">message</label><br>
-                    <textarea id="message" name="message"></textarea>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type = "file", name = "upload_image">
-                    <input type="submit" name="btn_submit" value="書き込む">
-                    <?php if( !empty($success_message) ): ?>
-                        <p class="success_message"><?php echo $success_message; ?></p>
-                    <?php endif; ?>
-                </td>
-            </tr>
+            <tbody id="form-box0">
+                <tr>
+                    <td>
+                        <label for="view_name">name</label>
+                        <input id="view_name" type="text" name="view_name" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="message">message</label><br>
+                        <textarea id="message" name="message" style="width:100%;"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type = "file", name = "upload_image">
+                        <input type="submit" name="btn_submit" value="書き込む">
+                        <?php if( !empty($success_message) ): ?>
+                            <p class="success_message"><?php echo $success_message; ?></p>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </form>
     <!-- ここにメッセージの入力フォームを設置 -->
