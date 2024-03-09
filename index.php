16a401b3e0b96db453756a061d482194c5d2e8ad
夏純心 <junshin@XiaTianMacBook-Air.local>
2024-03-09 12:06:54 +0900

    feature:掲示板見た目調整

--- index.php
+++ index.php
@@ -89,79 +89,89 @@ if( $file_handle = fopen(FILENAME,'r') ) {
 <meta charset="utf-8">
 <title>ひと言掲示板</title>
 <style>
+    #innerBody{margin:auto;position:relative;top:360px;/*background:red;*/}
+    #form-box{height:300px;background:#FFF;width:95%;right:0;left:0;margin:auto;bottom:30px;position:relative;border:solid 4px #000;border-radius:40px;}
     #box{overflow-y:scroll;height:700px;background:#FFF;width:95%;right:0;left:0;margin:auto;top:10px;position:relative;border:solid 4px #000;border-radius:40px;}
     #moveBtn{
-        font-size:40px;color:#FFF;background-color:#00CCCC;
-        right:0;left:35%;bottom:65px;margin:auto;padding:5px;position:relative;border-radius:100%;text-align:center;height:52px;width:52px;z-index:2;}
+        font-size:75px;color:#FFF;background-color:#00CCCC;
+        right:0;left:35%;bottom:65px;margin:auto;padding:5px;position:relative;border-radius:100%;text-align:center;height:92px;width:92px;z-index:2;}
     .showImg{width:95%;}
     .time{font-size:20px;background:#ffcc00;}
     .lineHeight{line-height:0.5px;}
     table{border-bottom:solid 2px #000; /*background:red;*/}
     td{flex-wrap: wrap;}
     .box0{width:95%; position:relative; right:0; left:0; margin:auto;/*background:skyblue;*/}
-    .td0{width:15%; /*background:red;*/} /*time*/
-    .td1{width:65%; /*background:yellow;*/} /*msg*/
+    .td0{width:15%; font-size:24px; /*background:red;*/} /*time*/
+    .td1{width:65%; /*background:yellow;*/} /*msg*/ .msg{font-size:30px;}
     .td2{width:20%; text-align:center; /*background:green;*/} /*photo*/
     .usrName{line-height:1px;}
 </style>
 </head>
 <body>
-<h1>ひと言掲示板</h1>
-<?php if( !empty($success_message) ): ?>
-    <p class="success_message"><?php echo $success_message; ?></p>
-<?php endif; ?>
-<?php if( !empty($error_message) ): ?>
-	<ul class="error_message">
-		<?php foreach( $error_message as $value ): ?>
-			<li><?php echo $value; ?></li>
-		<?php endforeach; ?>
-	</ul>
-<?php endif; ?>
-<form method="post", enctype = "multipart/form-data">
-	<div>
-		<label for="view_name">表示名</label>
-		<input id="view_name" type="text" name="view_name" value="">
-	</div>
-	<div>
-		<label for="message">ひと言メッセージ</label>
-		<textarea id="message" name="message"></textarea>
-	</div>
-    <div>
-    <input type = "file", name = "upload_image">
-    </div>
-	<input type="submit" name="btn_submit" value="書き込む">
-</form>
-<!-- ここにメッセージの入力フォームを設置 -->
-<hr>
-<div id="box">
-    <!-- ここに投稿されたメッセージを表示 -->
-    <div class="box0">
-        <?php if( !empty($message_array) ): ?>
-        <?php foreach( $message_array as $value ): ?>
-            <table>
-                <!--<h2><?php echo $value['view_name']; ?></h2>-->
-                <td class="td0">
-                    <p><?php echo date('m月d日', strtotime($value['post_date'])); ?></p>
-                    <p class="lineHeight"><?php echo date('H:i', strtotime($value['post_date'])); ?></p>
+<div id="innerBody">
+    <?php if( !empty($error_message) ): ?>
+        <ul class="error_message">
+            <?php foreach( $error_message as $value ): ?>
+                <li><?php echo $value; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+    <form method="post", enctype = "multipart/form-data">
+        <table id="form-box">
+            <tr>
+                <td>
+                    <label for="view_name">name</label>
+                    <input id="view_name" type="text" name="view_name" value="">
                 </td>
-                <td class="td1">
-                    <h2 class="usrName"><?php echo $value['view_name']; ?></h2>
-                    <?php echo $value['message']; ?>
-                </td><!--メッセージを表示しているところ-->
-                <!--画像を表示している箇所-->
-                <td class="td2">
-                    <!--<?php if(!empty($MSG)) echo $MSG;?>--><!--画像のファイル名, 後で削除-->
+            </tr>
+            <tr>
+                <td>
+                    <label for="message">message</label><br>
+                    <textarea id="message" name="message"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type = "file", name = "upload_image">
+                    <input type="submit" name="btn_submit" value="書き込む">
+                    <?php if( !empty($success_message) ): ?>
+                        <p class="success_message"><?php echo $success_message; ?></p>
+                    <?php endif; ?>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <!-- ここにメッセージの入力フォームを設置 -->
+    <div id="box">
+        <!-- ここに投稿されたメッセージを表示 -->
+        <div class="box0">
+            <?php if( !empty($message_array) ): ?>
+            <?php foreach( $message_array as $value ): ?>
+                <table>
+                    <td class="td0"><!--投稿時間-->
+                        <p><?php echo date('m月d日', strtotime($value['post_date'])); ?></p>
+                        <p class="lineHeight"><?php echo date('H:i', strtotime($value['post_date'])); ?></p>
+                    </td>
+
+                    <td class="td1"><!--メッセージを表示しているところ-->
+                        <h1 class="usrName"><?php echo $value['view_name']; ?></h1>
+                        <p class="msg"><?php echo $value['message']; ?></p>
+                    </td>
+
+                    <td class="td2"><!--画像を表示している箇所-->
+                        <!--<?php if(!empty($MSG)) echo $MSG;?>--><!--画像のファイル名, 後で削除-->
                         <?php if(!empty($img_path)){;?>
                             <img class="showImg" src = "<?php echo $value['img_data'];?>" alt="">
                         <?php }; ?>
-                </td>
-            </table>
-        <?php endforeach; ?>
-        <?php endif; ?>
+                    </td>
+                </table>
+            <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
 
-<p id="moveBtn" onclick="moveTop()">↑</p><!--スクロール用ボタン-->
+    <p id="moveBtn" onclick="moveTop()">↑</p><!--スクロール用ボタン-->
+</div>
 <script>
     let box=document.getElementById("box");
     let topBtn=class{
